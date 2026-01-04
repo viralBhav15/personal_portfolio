04382687e4fcb5e0f261812989201ef3b4cc5894
Viral Bhavsar <viralkum@ualberta.ca>
2026-01-03 17:00:10 -0700

    Hero Work

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,25 @@
 export const Hero = () => {
-    return <section></section>;
+    return <section className="realtive min-h-screen flex items-center overflow-hidden">
+        {/* Background */}
+        <div className="absolute inset-0">
+            <img 
+                src="/Hero.jpg"
+                alt="Hero Image"
+                className="w-full h-full object-cover opacity-40"
+            />
+            <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background"/>
+        </div>
+
+        {/* Green Dots */}
+        <div>
+            {[...Array(30)].map((_, i) => (
+                <div 
+                    className="absolute w-1.5 h-1.5 rounded-full opacity-60"
+                    style={{
+                        backgroundColor: "#20B2A6",
+                    }}
+                />
+            ))}
+        </div>
+    </section>;
 };
